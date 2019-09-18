1df62911edada653cd1044154d83011a441a6204
uguryuce <uguryucae1903@gmail.com>
2019-09-18 14:07:58 +0300

    +

---
+++ booster/navbar/script.js
@@ -0,0 +1,8 @@
+var $page = $('.page');
+
+        $('.menu_toggle').on('click', function() {
+            $page.toggleClass('shazam');
+        });
+        $('.content').on('click', function() {
+            $page.removeClass('shazam');
+        });
